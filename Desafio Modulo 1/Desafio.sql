9a113508e3fe12592235adc0e721b293ec2035da
Eric Dias Souza <ericrlg@hotmail.com>
2020-07-24 20:08:19 -0300

    -m correcoes

--- Desafio Modulo 1/Desafio.sql
+++ Desafio Modulo 1/Desafio.sql
@@ -10,19 +10,18 @@ Considere o banco de dados do enunciado.
 Crie uma consulta em SQL e responda: 
 Quantos empregados do departamento 5 trabalham mais de 10h por semana no projeto chamado "ProductX"?
 */
-SELECT DISTINCT COUNT(e) || ' Empregados trabalham mais de 10 horas no PRODUCTX' AS comentario
-FROM empregado e JOIN departamento d ON (d.dnumero = e.dno)
-JOIN trabalha_em t ON (t.essn = e.ssn)
-JOIN projeto p ON (p.pnumero = t.pno)
-WHERE d.dnumero = 5 AND UPPER(TRIM(p.pjnome)) = 'PRODUCTX'
-AND t.horas > 10
+SELECT COUNT(*) FROM empregado, trabalha_em, projeto
+WHERE dno=5
+AND essn = esnn AND pno= pnumero
+AND horas > 10
+AND pjnome= 'PRODUCTX'
 /*
 Pergunta 10
 Considere o banco de dados do enunciado. 
 Crie uma consulta em SQL e responda: Quantos empregados possuem um dependente com o mesmo primeiro nome que o deles?
 */
-SELECT COUNT(e) FROM dependente d JOIN empregado e ON (d.essn = e.ssn)
-WHERE UPPER(TRIM(d.nome_dependente)) = UPPER(TRIM(e.pnome)))
+SELECT COUNT(*) FROM empregado, dependente	
+WHERE essn = ssn AND pnome = nome_dependente
 /*
 Pergunta 11
 Considere o banco de dados do enunciado. 
@@ -30,11 +29,9 @@ Crie uma consulta em SQL e responda: Quem é a pessoa que possui mais tempo de a
 Considere o banco de dados do enunciado. 
 Crie uma consulta em SQL e responda: Quais os nomes de todos os empregados que são diretamente supervisionados por Franklin Wong
 */
-SELECT e.* FROM empregado e
-WHERE e.superssn  = (SELECT e.ssn
-                  FROM empregado e JOIN departamento d ON (d.dnumero = e.dno)
-				  WHERE UPPER(TRIM(e.pnome)) = UPPER(TRIM('Franklin')) 
-				  AND UPPER(TRIM(unome)) = UPPER(TRIM('Wong')))
+SELECT e. pnome
+WHERE ger.ssn = e.superssn
+AND ger.pnome= 'Franklin' AND ger.unome ='Wong'
 /*
 Pergunta 12
 Considere o banco de dados do enunciado. 
